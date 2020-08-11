a92171b9312207dac90a9c3e37921671c02dcd94
Mark Young <markyaaronyoung@gmail.com>
2020-08-10 21:40:48 -0500

    added an array of student objects into studentData.js

---
+++ src/helpers/data/studentsData.js
@@ -0,0 +1,132 @@
+const students = [
+  {
+    id: 'student1',
+    firstName: 'Austin',
+    lastName: 'Phy',
+    isDead: false,
+  },
+  {
+    id: 'student2',
+    firstName: 'Brooke',
+    lastName: 'Nemchak',
+    isDead: false,
+  },
+  {
+    id: 'student3',
+    firstName: 'Gene',
+    lastName: 'McNew',
+    isDead: false,
+  },
+  {
+    id: 'student4',
+    firstName: 'Gwynne',
+    lastName: 'Meeks',
+    isDead: false,
+  },
+  {
+    id: 'student5',
+    firstName: 'Jeanine',
+    lastName: 'Beckle',
+    isDead: false,
+  },
+  {
+    id: 'student6',
+    firstName: 'Jeremiah',
+    lastName: 'Vasquez',
+    isDead: false,
+  },
+  {
+    id: 'student7',
+    firstName: 'Jim',
+    lastName: 'Browning',
+    isDead: false,
+  },
+  {
+    id: 'student8',
+    firstName: 'Jonathan',
+    lastName: 'Shearon',
+    isDead: false,
+  },
+  {
+    id: 'student9',
+    firstName: 'Joshua',
+    lastName: 'Medlan',
+    isDead: false,
+  },
+  {
+    id: 'student10',
+    firstName: 'Kamiran',
+    lastName: 'Ibrahim',
+    isDead: false,
+  },
+  {
+    id: 'student11',
+    firstName: 'Mark',
+    lastName: 'Young',
+    isDead: false,
+  },
+  {
+    id: 'student11',
+    firstName: 'Matt',
+    lastName: 'Logan',
+    isDead: false,
+  },
+  {
+    id: 'student12',
+    firstName: 'Michael',
+    lastName: 'Dotson',
+    isDead: false,
+  },
+  {
+    id: 'student13',
+    firstName: 'Nate',
+    lastName: 'Owens',
+    isDead: false,
+  },
+  {
+    id: 'student14',
+    firstName: 'Nick',
+    lastName: 'Walters',
+    isDead: false,
+  },
+  {
+    id: 'student15',
+    firstName: 'Nikhil',
+    lastName: 'Gaikward',
+    isDead: false,
+  },
+  {
+    id: 'student16',
+    firstName: 'Ola',
+    lastName: 'Oladinni',
+    isDead: false,
+  },
+  {
+    id: 'student17',
+    firstName: 'Pete',
+    lastName: 'Stewart',
+    isDead: false,
+  },
+  {
+    id: 'student18',
+    firstName: 'Phonesalo',
+    lastName: 'Sihanorak',
+    isDead: false,
+  },
+  {
+    id: 'student19',
+    firstName: 'Ryan',
+    lastName: 'Beiden',
+    isDead: false,
+  },
+  {
+    id: 'student20',
+    firstName: 'William',
+    lastName: 'Campbell',
+    isDead: false,
+  },
+];
+
+const getStudents = () => students;
+
+export default { getStudents };
