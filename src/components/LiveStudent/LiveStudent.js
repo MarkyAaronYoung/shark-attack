8f72a03b21a27d5e31ccbc14c7cdee6dc3cb272c
Mark Young <markyaaronyoung@gmail.com>
2020-08-15 18:31:04 -0500

    added tombstone and fish pictures to graveyard/shark-tank

--- src/components/LiveStudent/LiveStudent.js
+++ src/components/LiveStudent/LiveStudent.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 // import PropTypes from 'prop-types';
@@ -13,11 +14,12 @@ class LiveStudent extends React.Component {
     const { student } = this.props;
 
     return (
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title text-uppercase">{student.firstName} {student.lastName}</h5>
-        </div>
-      </div>
+      <div className="card bg-dark text-light border-0">
+          <img className="card-img-top" src="https://images.theconversation.com/files/299379/original/file-20191030-154716-1wc4d64.jpg?ixlib=rb-1.1.0&rect=0%2C12%2C2048%2C1023&q=45&auto=format&w=1356&h=668&fit=crop" alt="one specific fish" />
+        <div className="card-img-overlay">
+              <h5 className="card-title text-uppercase">{student.firstName} {student.lastName}</h5>
+            </div>
+          </div>
     );
   }
 }
